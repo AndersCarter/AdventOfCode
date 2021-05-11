5dc8e70149d3d3a0d2d9b990be0e56f76252babf
Andrew Carter <andrew.carter@dr-inc.com>
2021-05-11 14:47:07 -0500

    Finished 2015 Day 5

--- 2015/day5.rkt
+++ 2015/day5.rkt
@@ -34,6 +34,36 @@
                         (or has-excluded? (member (string prev-char current-char) (list "ab" "cd" "pq" "xy"))) ; Checks for excluded substrings
                         (+ i 1))]))
 
+; string -> bool
+; Determines if the string is nice.
+; A nice string has,
+; 1. A pair of any two letters that appears at least twice in the string without overlapping,
+;    Such as, "xyxy" (xy) or "aabcdefgaa" (aa) but not "aaa".
+; 2. At least one letter which repreasts with exactly one letter between them.
+;    Such as, "xyx", or "abcdefeghi" (efe).
+
+(check-expect (is-nice?-part2 "qjhvhtzxzqqjkmpb") #t) ; 'qj' appears twice and 'zxz'.
+(check-expect (is-nice?-part2 "xxyxx") #t)
+(check-expect (is-nice?-part2 "uurcxstgmygtbstg") #f) ; No repeat with a single letter between
+(check-expect (is-nice?-part2 "ieodomkazucvgmuy") #f) ; No pair appears twice
+
+(define (is-nice?-part2 str)
+  (cond [(< (string-length str) 3) #f] ; Can't satisfy either condition
+        [else (define first-pair (substring str 0 2))
+              (define second-pair (substring str 1 3))
+              (define pair-set (set first-pair))
+              (is-nice?-part2-recur str pair-set second-pair #f (char=? (string-ref str 0) (string-ref str 2)) 3)]))
+
+(define (is-nice?-part2-recur str pair-set pair-hold has-pair? has-repeat? i)
+  (cond [(>= i (string-length str)) (and has-pair? has-repeat?)]
+        [else (define new-pair (substring str (- i 1) (+ i 1)))
+              (is-nice?-part2-recur str
+                                    (set-add pair-set pair-hold) ; Add held pair to set
+                                    new-pair ; Current pair becomes held pair
+                                    (or has-pair? (set-member? pair-set new-pair))
+                                    (or has-repeat? (char=? (string-ref str (- i 2)) (string-ref str i)))
+                                    (+ i 1))]))
+
 ; char -> bool
 ; Returns true if the character is a vowel (aeiou).
 (check-expect (is-vowel? #\a) #t)
@@ -52,3 +82,7 @@
 ; Part 1
 (for/sum ([str day5-input])
   (if (is-nice? str) 1 0))
+
+; Part 2
+(for/sum ([str day5-input])
+  (if (is-nice?-part2 str) 1 0))
